68d0a6937d8f9910a83d4109e81a5af118d7dc9e
maayanrab <maayan.lab@gmail.com>
2021-11-29 01:06:38 +0200

    Update settings.py

--- HikeBuddy/HikeBuddy/settings.py
+++ HikeBuddy/HikeBuddy/settings.py
@@ -128,3 +128,6 @@ CRISPY_TEMPLATE_PACK="bootstrap4"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
