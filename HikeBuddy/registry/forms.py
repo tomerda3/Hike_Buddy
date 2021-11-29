6855b8eeff3282d5d41946fc203c1d711be3ece7
maayanrab <maayan.lab@gmail.com>
2021-11-29 09:49:58 +0200

    Update forms.py

--- HikeBuddy/registry/forms.py
+++ HikeBuddy/registry/forms.py
@@ -5,7 +5,13 @@ from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
+    # userType = forms.CharField(label='User type', max_length=100)
+    USER_TYPES = [('traveler', 'Traveler'),
+    ('guide', 'Guide'),
+    ('host', 'Host'),]
+    userType = forms.CharField(label='User type', widget=forms.Select(choices=USER_TYPES))
     
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        # fields = ["username", "email", "password1", "password2", "userType"]
+        fields = ["username", "userType", "email", "password1", "password2"]
