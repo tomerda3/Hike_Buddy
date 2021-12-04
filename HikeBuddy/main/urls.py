19a8a934cca965c4fa69eaa18ea677c06212fcf1
shaima7 <shaima7@ac.sce.ac.il>
2021-12-04 16:52:47 +0200

    Update urls.py

--- HikeBuddy/main/urls.py
+++ HikeBuddy/main/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path,include
 
 from . import views
 
 urlpatterns = [
 path("", views.home, name="home"),
 path("profile/", views.profile, name="profile"),
-# path("home/", views.home, name="home"),
+
+    # path("home/", views.home, name="home"),
 # path("<str:name>", views.index, name="name"),
 ]
