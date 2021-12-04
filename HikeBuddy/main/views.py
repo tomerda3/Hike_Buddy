e576c1ba8eec537a7cfe6dca5ea075a2e5b930fb
shaima7 <shaima7@ac.sce.ac.il>
2021-12-04 16:52:49 +0200

    Update views.py

--- HikeBuddy/main/views.py
+++ HikeBuddy/main/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+
+
 # Create your views here.
 
 
 def home(response):
     return render(response, "main/home.html", {})
 
+
 def profile(response):
     return render(response, "main/profile.html", {})
+
 
 def toggle_active(response):
     user = User.objects.get(pk=response.user.id)
@@ -15,3 +19,4 @@ def toggle_active(response):
     user.save()
     redirect("/")
     return render(response, "main/home.html", {})
+
