06a521f2321e3190167433da85aa0c7908dbdf81
tomerda3 <93253870+tomerda3@users.noreply.github.com>
2021-12-05 13:00:44 +0200

    Update views.py

--- HikeBuddy/main/views.py
+++ HikeBuddy/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 
@@ -20,3 +21,23 @@ def toggle_active(response):
     redirect("/")
     return render(response, "main/home.html", {})
 
+
+def feedback(request):
+
+	if request.method == 'POST':
+		message = request.POST['message']
+
+		send_mail('Contact Form',
+		 message,
+		 settings.EMAIL_HOST_USER,
+		 ['HikeBuddy100@gmail.com'],
+		 fail_silently=False)
+	return render(request, 'main/thankyou.html')
+
+def about(response):
+    return render(response, "main/about.html", {})
+
+def contact(response):
+    # return render(response, "main/contact.html", {})
+    print("CONTACT")
+    return render(response, "main/contact.html", {})
