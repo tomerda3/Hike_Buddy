5fcd2883d1cdd3e447255555175fd66172974e44
tomerda3 <93253870+tomerda3@users.noreply.github.com>
2021-12-05 13:00:42 +0200

    Update urls.py

--- HikeBuddy/main/urls.py
+++ HikeBuddy/main/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
 path("", views.home, name="home"),
 path("profile/", views.profile, name="profile"),
+path('contact/', views.contact, name="contact"),
+path('about/', views.about, name="about"),
 
     # path("home/", views.home, name="home"),
 # path("<str:name>", views.index, name="name"),
