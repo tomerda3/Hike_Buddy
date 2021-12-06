383001ea9acae3e521926b37352c679f946687ae
maayanrab <maayara@ac.sce.ac.il>
2021-12-06 15:42:26 +0200

    Update views.py

--- HikeBuddy/registry/views.py
+++ HikeBuddy/registry/views.py
@@ -12,8 +12,17 @@ def signup(response):
             Group.objects.get_or_create(name=form.cleaned_data['userType'])
             cur_group = Group.objects.get(name=form.cleaned_data['userType'])
             cur_group.user_set.add(user)
-            print(form.cleaned_data['phone'])  # Not yet really saved
+
+            user.userType = form.cleaned_data['userType']
+            user.email = form.cleaned_data['email']
+            user.first_name = form.cleaned_data['first_name']
+            user.last_name = form.cleaned_data['last_name']
+            user.phone = form.cleaned_data['phone']
+
+            user.save()
+
             return redirect("/login/")
     else:
         form = RegisterForm()
+        return redirect("/login/")
     return render(response, "registry/signup.html", {"form":form})
