29d9864778c154b15bb9c7005aa7efd157ff095c
maayanrab <maayara@ac.sce.ac.il>
2021-12-23 00:32:19 +0200

    Update settings.py

--- HikeBuddy/HikeBuddy/settings.py
+++ HikeBuddy/HikeBuddy/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_DIR= Path.joinpath(BASE_DIR,"static")
 
 
 # Quick-start development settings - unsuitable for production
@@ -140,3 +141,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
