f9dbf0f03f3db97c45cc5f1bd7bbe109c3974881
maayanrab <maayan.lab@gmail.com>
2021-12-22 15:46:22 +0200

    Update urls.py

--- HikeBuddy/main/urls.py
+++ HikeBuddy/main/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import path,include
 
 from . import views
 
 urlpatterns = [
 path("", views.home, name="home"),
 path("profile/", views.profile, name="profile"),
+path("contact/", views.contact, name="contact"),
+path("about/", views.about, name="about"),
+path("areyousure/", views.areyousure, name="areyousure"),
+
 # path("home/", views.home, name="home"),
 # path("<str:name>", views.index, name="name"),
 ]
