6aa2f84b11e118166cbacd761e7ad3450320415f
tomerda3 <93253870+tomerda3@users.noreply.github.com>
2021-12-23 23:10:07 +0200

    Update admin.py

--- HikeBuddy/main/admin.py
+++ HikeBuddy/main/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 # from .models import ToDoList, Item
+from .models import HostingPlace
 
 # Register your models here.
 # admin.site.register(ToDoList)
 # admin.site.register(Item)
+
+admin.site.register(HostingPlace)
