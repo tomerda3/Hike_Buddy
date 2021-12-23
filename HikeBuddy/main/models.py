0dada1ee2ecd6e1ec170e8d27d77e9e8806f6f4b
tomerda3 <93253870+tomerda3@users.noreply.github.com>
2021-12-23 23:16:06 +0200

    Update models.py

--- HikeBuddy/main/models.py
+++ HikeBuddy/main/models.py
@@ -1,4 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django import forms
 
 # Create your models here.
+
+class HostingPlace(models.Model):
+
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    fireplace = models.BooleanField(default=False)
+    singleBeds = models.BooleanField(default=False)
+    doubleBeds = models.BooleanField(default=False)
+    freeWiFi = models.BooleanField(default=False)
+    showers = models.BooleanField(default=False)
+    electricity = models.BooleanField(default=False)
+    breakfast = models.BooleanField(default=False)
+    airConditioning = models.BooleanField(default=False)
+    parking = models.BooleanField(default=False)
+    bar = models.BooleanField(default=False)
+    picture = models.ImageField(upload_to='static\\media\\host_pics', blank=True)
+
+
+
+    def __str__(self):
+        return self.name
