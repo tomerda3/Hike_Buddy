8eae30b536d9c2c523b555a1355a46578e33ca7b
maayanrab <maayan.lab@gmail.com>
2021-12-29 17:30:21 +0200

    Update urls.py

--- HikeBuddy/main/urls.py
+++ HikeBuddy/main/urls.py
@@ -4,11 +4,17 @@ from . import views
 
 urlpatterns = [
 path("", views.home, name="home"),
-path("profile/", views.profile, name="profile"),
 path("contact/", views.contact, name="contact"),
 path("about/", views.about, name="about"),
 path("areyousure/", views.areyousure, name="areyousure"),
 path("planroute/", views.planroute, name="planroute"),
+path("findhost/", views.findhost, name="findhost"),
 path("myhostingplaces/", views.myhostingplaces, name="myhostingplaces"),
+path("createhostingplace/", views.createhostingplace, name="createhostingplace"),
 path("createHost/", views.createHost, name="createHost"),
+path("guideinfo/", views.guideinfo, name="guideinfo"),
+path("createGuide/", views.createGuide, name="createGuide"),
+path("profile/<username>", views.profile, name="profile"),
+path("profile/", views.myprofile, name="myprofile"),
+path("addroute/<route>", views.addroute, name="addroute"),
 ]
