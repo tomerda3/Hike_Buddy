010f5c59b8ae30c68a4defeb89dea519bfbd70f6
maayanrab <maayara@ac.sce.ac.il>
2021-12-30 15:19:52 +0200

    Update filters.py

--- HikeBuddy/main/filters.py
+++ HikeBuddy/main/filters.py
@@ -11,3 +11,13 @@ class HostingPlaceFilter(django_filters.FilterSet):
 		model = HostingPlace
 		fields = '__all__'
 		exclude = ['username']
+
+
+class GuideInfoFilter(django_filters.FilterSet):
+	username = CharFilter(field_name='username', lookup_expr='icontains')
+	routes = CharFilter(field_name='routes', lookup_expr='icontains')
+
+	class Meta:
+		model = GuideInfo
+		fields = '__all__'
+		exclude = ['username']
