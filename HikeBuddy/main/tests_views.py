77890f9d74ca5b9e65c12bd0d3d4026e0b4be2d2
shaima7 <shaima7@ac.sce.ac.il>
2022-01-04 14:51:55 +0200

    Update tests_views.py

--- HikeBuddy/main/tests_views.py
+++ HikeBuddy/main/tests_views.py
@@ -6,5 +6,50 @@ from main.forms import HostForm
 class TestsViews(TestCase):
 
     def test_createhostingplace(self):
-        response = self.client.get('/')
-        self.assertNotEqual(response.status_code, 404)
+        response = self.client.get('main/myhostingplaces.html')
+        self.assertNotEqual(response.status_code, 405)
+        self.assertEqual(response.status_code, 404)
+
+    def test_myhostingplaces(self):
+        response = self.client.get('main/myhostingplaces.html')
+        self.assertNotEqual(response.status_code, 405)
+        self.assertEqual(response.status_code, 404)
+
+    def test_home(self):
+        response = self.client.get('main/home.html')
+        self.assertEqual(response.status_code, 404)
+        self.assertNotEqual(response.status_code, 405)
+
+    def test_checkmontly(self):
+        self.assertNotEqual(self, False)
+
+    def test_myprofile(self):
+        response = self.client.get('main/myprofile.html')
+        self.assertNotEqual(response.status_code, 405)
+        self.assertEqual(response.status_code, 404)
+
+    def test_editabout(self):
+        response = self.client.get('main/editabout.html')
+        self.assertNotEqual(response.status_code, 405)
+        self.assertEqual(response.status_code, 404)
+
+    def test_messagetouser(self):
+        response = self.client.get('main/messagetouser.html')
+        self.assertNotEqual(response.status_code, 405)
+        self.assertEqual(response.status_code, 404)
+
+    def test_about(self):
+        response = self.client.get('main/about.html')
+        self.assertNotEqual(response.status_code, 405)
+        self.assertEqual(response.status_code, 404)
+
+    def test_contact(self):
+        response = self.client.get('main/contact.html')
+        self.assertNotEqual(response.status_code, 405)
+        self.assertEqual(response.status_code, 404)
+        self.assertNotEqual(response.status_code, 200)
+
+
+
+
+
