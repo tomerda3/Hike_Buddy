ef51030f807b03d834dde4a0c9e3f0701fa5497f
shaima7 <shaima7@ac.sce.ac.il>
2022-01-05 17:26:15 +0200

    Create tests_forms.py

---
+++ HikeBuddy/registry/tests_forms.py
@@ -0,0 +1,21 @@
+from django.test import SimpleTestCase
+from registry.forms import UserForm,UserProfileInfoForm
+
+
+class TestForms(SimpleTestCase):
+
+    def test_UserForm(self):
+        form = UserForm(data={
+            'password': '123',
+            'password_confirm': '123',
+        })
+        self.assertFalse(form.is_valid())
+
+    def test_UserProfileInfoForm(self):
+        form = UserProfileInfoForm(data={
+            'userType': '1',
+            'phone': '123',
+            'picture': False,
+        })
+        self.assertTrue(form.is_valid())
+
