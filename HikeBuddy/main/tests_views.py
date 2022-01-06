624603ae07198b146a8d9c4d302955b6b48ef880
shaima7 <shaima7@ac.sce.ac.il>
2022-01-06 18:58:46 +0200

    Update tests_views.py

--- HikeBuddy/main/tests_views.py
+++ HikeBuddy/main/tests_views.py
@@ -1,9 +1,28 @@
-from django.test import TestCase ,Client
-from main.views import areyousure,home,contact
+from django.test import TestCase, Client
+from main.views import areyousure, home, contact
 from django.urls import reverse, resolve
-from main.forms import HostForm
+
 
 class TestsViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.home_url = reverse('home')
+        self.feedback_url = reverse('feedback')
+        self.about_url = reverse('about')
+        self.contact_url = reverse('contact')
+        self.areyousure_url = reverse('areyousure')
+        self.createhostingplace_url = reverse('createhostingplace')
+        self.myhostingplaces_url = reverse('myhostingplaces')
+        self.createHost_url = reverse('createHost')
+
+
+
+
+
+
+
+
 
     def test_createhostingplace(self):
         response = self.client.get('main/myhostingplaces.html')
@@ -48,6 +67,43 @@ class TestsViews(TestCase):
         self.assertNotEqual(response.status_code, 405)
         self.assertEqual(response.status_code, 404)
         self.assertNotEqual(response.status_code, 200)
+
+    def test_home2(self):
+        response = self.client.get(self.home_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/home.html')
+
+    def test_feedback2(self):
+        response = self.client.get(self.feedback_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/thankyou.html')
+
+    def test_contact2(self):
+            response = self.client.get(self.contact_url)
+            self.assertEqual(response.status_code, 200)
+            self.assertTemplateUsed(response, 'main/contact.html')
+
+    def test_areyousure2(self):
+        response = self.client.get(self.areyousure_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/areyousure.html')
+
+    def test_createhostingplace2(self):
+        response = self.client.get(self.createhostingplace_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/createhostingplace.html')
+
+
+    def test_myhostingplaces2(self):
+        response = self.client.get(self.createhostingplace_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_createHost2(self):
+        response = self.client.get(self.createHost_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/myhostingplaces.html')
+
+
 
 
 
